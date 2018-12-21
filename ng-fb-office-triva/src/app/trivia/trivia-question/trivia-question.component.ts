f4fbd4e449db2c6c79e5dec290ccf71829e020bb
Kyle Henry <krhenry433@gmail.com>
2018-12-21 14:54:30 -0500

    Randomized list of questions when playing trivia.

--- ng-fb-office-triva/src/app/trivia/trivia-question/trivia-question.component.ts
+++ ng-fb-office-triva/src/app/trivia/trivia-question/trivia-question.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from 'src/app/questions/shared/question.service';
+import { Question } from 'src/app/questions/shared/question.model';
 
 @Component({
   selector: 'app-trivia-question',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trivia-question.component.css']
 })
 export class TriviaQuestionComponent implements OnInit {
+  questionList: Question[];
+  rndQuestionList: Question[];
+  currentQuestion: string;
+  count = 0;
+  random: number;
 
-  constructor() { }
+  constructor(private questionService: QuestionService) { }
 
   ngOnInit() {
+    const x = this.questionService.getData();
+    x.snapshotChanges().subscribe(item => {
+      this.questionList = [];
+      this.count = 0;
+      item.forEach(element => {
+        const y = element.payload.toJSON();
+        y['$key'] = element.key;
+        this.questionList.push(y as Question);
+        this.count += 1;
+      });
+      this.rndQuestionList = this.shuffle(this.questionList);
+      this.currentQuestion = this.rndQuestionList[0].question;
+    });
   }
 
+  shuffle(array) {
+    let m = array.length;
+
+    while (m) {
+      const i = Math.floor(Math.random() * m--);
+      const t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+  }
 }
--- ng-fb-office-triva/src/app/trivia/trivia.component.ts
+++ ng-fb-office-triva/src/app/trivia/trivia.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../questions/shared/question.service';
 
 @Component({
   selector: 'app-trivia',
   templateUrl: './trivia.component.html',
-  styleUrls: ['./trivia.component.css']
+  styleUrls: ['./trivia.component.css'],
+  providers: [QuestionService]
 })
 export class TriviaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private questionService: QuestionService) { }
 
   ngOnInit() {
   }
